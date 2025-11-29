a3dcf95905963decdf092c17c7d184e27395e07d
ziv halsband <zivhalsband@MacBook-Pro-sl-ziv.local>
2025-11-29 20:55:13 +0200

    migrateion added

---
+++ services/auth-service/prisma/migrations/20251129183823_add_oauth_support/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[provider,provider_id]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateEnum
+CREATE TYPE "AuthProvider" AS ENUM ('EMAIL', 'GOOGLE');
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "provider" "AuthProvider" NOT NULL DEFAULT 'EMAIL',
+ADD COLUMN     "provider_id" TEXT,
+ALTER COLUMN "password_hash" DROP NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_provider_provider_id_key" ON "users"("provider", "provider_id");
